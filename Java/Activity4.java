76f1e1ba77811f9bc748a1da34d8ef2ca2f1cb44
TahaseenTabassum <106802428+TahaseenTabassum@users.noreply.github.com>
2022-06-03 15:38:49 +0530

    Add files via upload

---
+++ Java/Activity4.java
@@ -0,0 +1,33 @@
+package activities;
+
+import java.util.Arrays;
+
+class Activity4 {
+    static void ascendingSort(int array[]) {
+        int size = array.length, i;
+        
+        for (i = 1; i < size; i++) {
+            int key = array[i];
+            int j = i - 1;
+            
+            while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
+                --j;
+            }
+            array[j + 1] = key;
+        }
+    }
+    
+    public static void main(String args[]) {
+        int[] data = { 9, 5, 1, 4, 3 };
+        System.out.println("Array before sorting: ");
+        for(int i=0;i<data.length;i++)
+        {
+        	 System.out.println(data[i]);
+        }
+       
+        ascendingSort(data);
+        System.out.println("Sorted Array in Ascending Order: ");
+        System.out.println(Arrays.toString(data));
+    }
+}
